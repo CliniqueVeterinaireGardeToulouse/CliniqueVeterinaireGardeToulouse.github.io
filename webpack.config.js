f11a928cd8cd6548ac390068a08427351d5df9d2
djfm <fm.de.jouvencel@gmail.com>
2017-09-09 01:03:25 +0200

    add scss compiling

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,18 @@ module.exports = {
     filename: 'bundle.js',
     path: __dirname,
   },
+  module: {
+    rules: [{
+      test: /\.scss$/,
+      use: [{
+        loader: 'style-loader',
+      }, {
+        loader: 'css-loader',
+      }, {
+        loader: 'sass-loader',
+      }],
+    }],
+  },
   devServer: {
     contentBase: __dirname,
     compress: true,
