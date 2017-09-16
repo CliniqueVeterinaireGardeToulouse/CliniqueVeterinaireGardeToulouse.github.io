4c570cdb2f0bf21e74bc8613be93295ae3fe4127
djfm <fm.de.jouvencel@gmail.com>
2017-09-16 21:34:04 +0200

    add phone number

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,11 +4,13 @@ const Home = () => (
   <div>
     <header>
       <img alt="" src="/logo.png" />
+      <a href="tel:0561112131"><strong>05 61 11 21 31</strong></a>
     </header>
     <main>
       <p>
-        Nous prenons en charge vos urgences vétérinaires lorsque votre
-        vétérinaire habituel n{'\''}est pas disponible.
+        Nous prenons en charge vos{' '}
+        <strong>urgences vétérinaires</strong>{' '}
+        lorsque votre vétérinaire habituel n{'\''}est pas disponible.
       </p>
     </main>
   </div>
