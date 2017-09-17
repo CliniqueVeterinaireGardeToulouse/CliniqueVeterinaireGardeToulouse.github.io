9ab8f033efdf0b615e264e60710abfa7402e922d
djfm <fm.de.jouvencel@gmail.com>
2017-09-17 15:27:47 +0200

    remove generated CSS during development
    In production, the css is extracted
    by the ExtractTextPlugin, so no need to require
    the scss either.

--- src/index.jsx
+++ src/index.jsx
@@ -3,8 +3,6 @@
 import React from 'react';
 import { render as RenderIntoDOM } from 'react-dom';
 
-import './style/index.scss';
-
 const render = () => {
   // eslint-disable-next-line global-require
   const App = require('./App').default;
@@ -15,7 +13,15 @@ const whenDocumentIsReady = (fn) => {
   document.addEventListener('DOMContentLoaded', fn);
 };
 
-whenDocumentIsReady(render);
+whenDocumentIsReady(() => {
+  render();
+  if (process.env.NODE_ENV !== 'production') {
+    Array.from(document.getElementsByTagName('style')).forEach(
+      styleNode => styleNode.parentNode.removeChild(styleNode)
+    );
+    require('./style/index.scss');
+  }
+});
 
 if (module.hot) {
   module.hot.accept('./App', () => render());
